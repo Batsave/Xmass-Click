75abf1c554b1fac0dfdfe291d7df9cf1196bf421
Defreitasnicolas <defreitasnicolas@gmail.com>
2023-11-23 16:08:17 +0100

    ajout des link vers github

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import ErrorPage from "./pages/404";
 import { WildCoinProvider } from "./components/WildCoin/WildCoinContext";
 import WildCoin from "./components/WildCoin/WildCoin";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
