1f13ac63c82cb32282e21c2e5799b1f0a0870714
batsave <batsave.twitch@gmail.com>
2023-11-23 15:33:25 +0100

    👌 IMPROVE: Card Done

--- src/components/BoutiqueCard.jsx
+++ src/components/BoutiqueCard.jsx
@@ -1,24 +1,30 @@
-export default function BoutiqueCard() {
+import "../scss/components/boutiquecard.scss";
+import PrimaryBtn from "./buttons/PrimaryButton";
 
+export default function BoutiqueCard() {
+  let backgroundImage =
+    "url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Sign-check-icon.png/768px-Sign-check-icon.png')";
   return (
-    <div style={{backgroundImage:url(hello)}} className="card">
-      <div className="container">
-        <div className="cardpicture" />
+    <div className="shopcardcontainer">
+      <div className="shopcontainer">
+        <div
+          className="cardpicture"
+          style={{ backgroundImage: `${backgroundImage}` }}
+        />
         <div>
           <div className="titlesection">
-            <p className="Itemname">Item name</p>
+            <p className="itemname">Item name</p>
             <div className="price">
-              <p className="Itemprice">Item price</p>
-              <div className="pricecard" />
+              <p className="itemprice">Item price</p>
+              <div className="priceicon" />
             </div>
           </div>
           <div className="description">
-            <p className="Itemdescription">Item description</p>
+            <p className="itemdesc">Item description</p>
           </div>
-          <button className="buttoncard" type="button">
-            Acheter
-          </button>
+          
         </div>
+        <PrimaryBtn className="buttoncard" btnText="Acheter" btnLink="/#" />
       </div>
     </div>
   );
