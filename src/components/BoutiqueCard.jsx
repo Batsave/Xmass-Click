c3d835378cea699844012d723921707b35408dc7
batsave <batsave.twitch@gmail.com>
2023-11-23 16:14:09 +0100

    🐛 FIX: DONE

--- src/components/BoutiqueCard.jsx
+++ src/components/BoutiqueCard.jsx
@@ -1,30 +1,43 @@
 import "../scss/components/boutiquecard.scss";
 import PrimaryBtn from "./buttons/PrimaryButton";
+import PropTypes from "prop-types";
 
-export default function BoutiqueCard() {
-  let backgroundImage =
-    "url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Sign-check-icon.png/768px-Sign-check-icon.png')";
+export default function BoutiqueCard({
+  name,
+  price,
+  description,
+  image,
+  link,
+}) {
+  BoutiqueCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+  };
+
   return (
     <div className="shopcardcontainer">
       <div className="shopcontainer">
         <div
           className="cardpicture"
-          style={{ backgroundImage: `${backgroundImage}` }}
+          style={{ backgroundImage: `url(${image})` }}
+          alt={`image de ${name}`}
         />
         <div>
           <div className="titlesection">
-            <p className="itemname">Item name</p>
+            <p className="itemname">{name}</p>
             <div className="price">
-              <p className="itemprice">Item price</p>
+              <p className="itemprice">{price}</p>
               <div className="priceicon" />
             </div>
           </div>
           <div className="description">
-            <p className="itemdesc">Item description</p>
+            <p className="itemdesc">{description}</p>
           </div>
-          
         </div>
-        <PrimaryBtn className="buttoncard" btnText="Acheter" btnLink="/#" />
+        <PrimaryBtn className="buttoncard" btnText="Acheter" btnLink={link} />
       </div>
     </div>
   );
