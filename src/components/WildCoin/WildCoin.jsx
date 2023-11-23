2da43fced901e0230a18dc6b9f2418fbd66816e6
Tetardtek <kvnn64@gmail.com>
2023-11-23 12:13:01 +0100

    fix WildCoin

--- src/components/WildCoin/WildCoin.jsx
+++ src/components/WildCoin/WildCoin.jsx
@@ -1,15 +1,27 @@
 import { useWildCoin } from "./WildCoinContext";
 import WildCoinIncrementAction from "./WildCoinIncrementAction";
+import Ameliorations from "./Amelioration";
 
 function WildCoin() {
   const { wildCoin } = useWildCoin();
+  const { incrementClick, incrementPerSecond } = useWildCoin();
 
   return (
-    <div className="WildCoin">
-      <h2>WildCoin</h2>
-      <p>WildCoin : {wildCoin}</p>
-      <WildCoinIncrementAction />
-    </div>
+    <>
+      <div className="WildCoin">
+        <h2>WildCoin</h2>
+        <p>WildCoin : {wildCoin}</p>
+        <WildCoinIncrementAction />
+        <p className="tetardCoinClick">
+          Taux de génération actif : Lv.{incrementClick} par onClick
+        </p>
+
+        <p className="tetardCoinPassif">
+          Taux de génération passif : Lv.{incrementPerSecond} par seconde
+        </p>
+        <Ameliorations />
+      </div>
+    </>
   );
 }
 
