5b645024f365092d491bc6217d9953d61806db39
Sébastien LAMBRANCA <116831255+Lambseb@users.noreply.github.com>
2023-11-24 03:34:09 +0100

    feat: random card by number.id

--- src/components/AchievementsCard.jsx
+++ src/components/AchievementsCard.jsx
@@ -1,20 +1,21 @@
 import "../scss/components/achievementscard.scss";
 import PropTypes from "prop-types";
 
-function AchievementsCard({ name, description, image }) {
+function AchievementsCard({ name, description, image, key }) {
   AchievementsCard.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
+    key: PropTypes.number.isRequired,
   };
+
   return (
-    <div className="achievCardcontainer">
-        <img
-          className="achievecardpicture" src={image} alt="cartes speciales"/>
-          <div className="achievetitle">
-            <p className="achievname">{name}</p>
-            <p className="achievdescription">{description}</p>
-        </div>
+    <div className="achievCardcontainer" key={key}>
+      <img className="achievecardpicture" src={image} alt="cartes speciales" />
+      <div className="achievetitle">
+        <p className="achievname">{name}</p>
+        <p className="achievdescription">{description}</p>
+      </div>
     </div>
   );
 }
